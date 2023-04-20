f99464d7c741f14d49e7a3b8284b6798c17c830c
Canon Samson <samsoncanon2018@gmail.com>
2023-04-20 00:18:33 +0000

    viewport

--- src/page/LandingPage.jsx
+++ src/page/LandingPage.jsx
@@ -7,27 +7,27 @@ const LandingPage = () => {
     const [signPop, setsignPop] = useState(false)
     return (
         <div>
-            <div className=" gap-5 grid grid-cols-2 justify-around  min-h-screen w-full items-center px-10">
-                <div className=" flex flex-col gap-2">
-                    <h1 className=" text-5xl font-semibold">Recieve anonymous compliments from your friends.</h1>
+            <div className=" gap-5 grid sm:grid-cols-2 justify-around  min-h-screen w-full items-center sm:px-10 px-3">
+                <div className=" flex flex-col gap-2 w-full ">
+                    <h1 className=" text-3xl  sm:text-5xl font-semibold">Recieve anonymous compliments from your friends.</h1>
                     <p>Recieve anonymous compliments from your friends and send anonymous messages to your friends for free.</p>
                     <div className=" flex gap-2 text-[12px]">
                         <button
                             onClick={() => { setsignPop(!signPop) }}
-                            className=" bg-blue-600 text-white w-[150px] py-3  rounded
+                            className=" bg-blue-600 text-white min-w-[150px]  w-[150px] py-3  rounded
                             active:scale-95 duration-75 ">
                             GET STARTED
                         </button>
                         <button
                             onClick={() => { setLoginPop(!loginPop) }}
-                            className=" border-2 border-blue-600 text-black w-[150px] py-3
+                            className=" border-2 border-blue-600 text-black min-w-[150px] w-[150px] h-[45px]
                      active:scale-95 duration-75  rounded">
                             LOG IN
                         </button>
 
                     </div>
                 </div>
-                <img src={hero} alt="" />
+                <img className=" hidden sm:flex" src={hero} alt="" />
             </div>
 
             <div>
